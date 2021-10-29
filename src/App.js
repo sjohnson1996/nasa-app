32cc9331e77c2478f0890505afad8a2537ddc52d
Scott Johnson <scott.johnson@daveramsey.com>
2021-10-29 13:52:57 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Space from './images/space.jpeg';
+import axios from 'axios';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=pHaZAi1sRICSKmqkNqLIieadVr15pl6OQ8xbTKD3`)
+    .then(data => setData(data));
+  }, [])
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        background: `url(${Space}) no-repeat center center fixed`,
+        width: '100vw',
+        height: '100vh',
+
+      }}
+    >
+      <h1 style={{ 
+        textAlign: 'center', 
+        padding: '10px', 
+        backgroundColor: '#58587a', 
+        color: 'white', 
+        margin: '0px', 
+      }}
+      >
+        Welcome To My Website
+      </h1>
+      <div
+        style={{
+          backgroundColor: 'white',
+          display:'flex',
+          flexWrap:'wrap'
+        }}
+      >
+        {(Object.keys(data).length) ?
+        <>
+          {data.data.photos.map((photo, i) => {
+          if (i < 50) return (
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={photo.img_src}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          );
+          console.log(photo.img_src)
+        })}
+      </>
+      :
+        null
+      }
+      </div>
     </div>
   );
 }
